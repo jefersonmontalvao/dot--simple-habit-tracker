72d3887cda9a34cc0cb08cc0d48d58fcea35a2ba
Jeferson Montalvão <76631154+jefersonmontalvao@users.noreply.github.com>
2025-12-04 01:10:08 -0300

    Create Habit.kt
    - Add Habit Domain Model

---
+++ app/src/main/java/com/example/dot__simple_habit_tracker/domain/model/Habit.kt
@@ -0,0 +1,15 @@
+package com.example.dot__simple_habit_tracker.domain.model
+
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
+import java.util.UUID
+
+data class Habit(
+    val id: String = UUID.randomUUID().toString(),
+    val name: String,
+    val creationDate: LocalDateTime = LocalDateTime.now(),
+) {
+    fun daysSinceCreation(): Long =
+        ChronoUnit.DAYS.between(creationDate, LocalDateTime.now()
+    )
+}
