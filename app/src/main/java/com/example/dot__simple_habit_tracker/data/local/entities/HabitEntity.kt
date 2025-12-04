3ae62bfb7ee8827a1878c7e169ea1877d7412efa
Jeferson Montalvão <76631154+jefersonmontalvao@users.noreply.github.com>
2025-12-04 11:40:55 -0300

    Implement and register LocalDateTime for room
    - Update HabitEntity.kt to use `LocalDateTime` fields
    - Update HabitMapper.kt to convert between domain model and entity
    - Add `LocalDateTimeConverters` for room

--- app/src/main/java/com/example/dot__simple_habit_tracker/data/local/entities/HabitEntity.kt
+++ app/src/main/java/com/example/dot__simple_habit_tracker/data/local/entities/HabitEntity.kt
@@ -2,10 +2,11 @@ package com.example.dot__simple_habit_tracker.data.local.entities
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.time.LocalDateTime
 
 @Entity(tableName = "habits")
 data class HabitEntity(
     @PrimaryKey val id: String,
     val name: String,
-    val creationDateIso: String
+    val creationDate: LocalDateTime
 )
--- app/src/main/java/com/example/dot__simple_habit_tracker/data/mappers/HabitMapper.kt
+++ app/src/main/java/com/example/dot__simple_habit_tracker/data/mappers/HabitMapper.kt
@@ -2,18 +2,17 @@ package com.example.dot__simple_habit_tracker.data.mappers
 
 import com.example.dot__simple_habit_tracker.data.local.entities.HabitEntity
 import com.example.dot__simple_habit_tracker.domain.models.Habit
-import java.time.LocalDateTime
 
 fun HabitEntity.toDomain(): Habit =
     Habit(
         id = id,
         name = name,
-        creationDate = LocalDateTime.parse(creationDateIso)
+        creationDate = creationDate
     )
 
 fun Habit.toEntity(): HabitEntity =
     HabitEntity(
         id = id,
         name = name,
-        creationDateIso = creationDate.toString()
+        creationDate = creationDate
     )
