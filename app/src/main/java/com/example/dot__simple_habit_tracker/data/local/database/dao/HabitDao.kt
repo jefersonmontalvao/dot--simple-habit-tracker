76f1b2925f2c49bd217893bb148a8145719482d9
Jeferson Montalvão <76631154+jefersonmontalvao@users.noreply.github.com>
2025-12-04 01:43:57 -0300

    Create HabitDao.kt
    - Add HabitDao interface

---
+++ app/src/main/java/com/example/dot__simple_habit_tracker/data/local/database/dao/HabitDao.kt
@@ -0,0 +1,24 @@
+package com.example.dot__simple_habit_tracker.data.local.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.dot__simple_habit_tracker.data.local.entities.HabitEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface HabitDao {
+    @Query("SELECT * FROM habits")
+    fun getAll(): Flow<List<HabitEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(habit: HabitEntity)
+
+    @Delete
+    suspend fun delete(habit: HabitEntity)
+
+    @Query("SELECT * FROM habits WHERE id = :id LIMIT 1")
+    suspend fun getById(id: String): HabitEntity?
+}
