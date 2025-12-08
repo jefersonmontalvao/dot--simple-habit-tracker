2b4c5027368279df9bd2e6a1b228ad73fdc65006
Jeferson Montalvão <76631154+jefersonmontalvao@users.noreply.github.com>
2025-12-08 10:26:54 -0300

    Fix HabitMapper

--- app/src/main/java/com/example/dot__simple_habit_tracker/data/mappers/HabitMapper.kt
+++ app/src/main/java/com/example/dot__simple_habit_tracker/data/mappers/HabitMapper.kt
@@ -1,7 +1,7 @@
 package com.example.dot__simple_habit_tracker.data.mappers
 
 import com.example.dot__simple_habit_tracker.data.local.entities.HabitEntity
-import com.example.dot__simple_habit_tracker.domain.models.Habit
+import com.example.dot__simple_habit_tracker.domain.model.Habit
 
 fun HabitEntity.toDomain(): Habit =
     Habit(
